697ccab8ac489af08c596ad60f2ac81fafe4a205
ahawker <andrew.r.hawker@gmail.com>
2013-02-06 20:19:16 -0800

    Basic project layout.

---
+++ jpool/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+
+from flask import flask
+
+app = Flask(__name__)
---
+++ jpool/shell.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com'
+
+import os
+import readline
+from pprint import pprint
+
+from flask import *
+from app import *
+
+os.environ['PYTHONINSPECT'] = 'True'
